0360df5cee9df86ef55c43f59da5897df6822a01
Melanie Vogel <melanie.vogel@siemens-healthineers.com>
2021-10-21 15:46:05 +0200

    feat: Added Disarium Number kata.

---
+++ CSharpKatas.Tests/7Kyu/DisariumNumberTests.cs
@@ -0,0 +1,26 @@
+﻿using CSharp_Katas._7Kyu;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp_Katas_Tests._7Kyu
+{
+    [TestClass]
+    public class DisariumNumberTests
+    {
+        [DataRow(89, "Disarium !!")]
+        [DataRow(564, "Not !!")]
+        [DataRow(1024, "Not !!")]
+        [DataRow(135, "Disarium !!")]
+        [DataTestMethod]
+        public void DisariumNumbers_Should_Return_Expected(int input, string expected)
+        {
+            // Arrange
+            // --> DataRow
+
+            // Act
+            var actual = DisariumNumber.DisariumNumbers(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ CSharpKatas/7Kyu/DisariumNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharp_Katas._7Kyu
+{
+    public class DisariumNumber
+    {
+        public static string DisariumNumbers(int number)
+        {
+            double result = 0;
+
+            var narr = number.ToString().ToCharArray();
+
+            for (int i = 0; i < narr.Length; i++)
+            {
+                var numberAsChar = narr[i];
+                var numberAsDouble = double.Parse(numberAsChar.ToString());
+                var exponentAsDouble = Convert.ToDouble(i + 1);
+
+                result += Math.Pow(numberAsDouble, exponentAsDouble);
+            }
+
+            if (result == number)
+            {
+                return "Disarium !!";
+            }
+            else
+            {
+                return "Not !!";
+            }
+        }
+    }
+}
