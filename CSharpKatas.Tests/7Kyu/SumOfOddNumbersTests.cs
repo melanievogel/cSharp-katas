922c736595b7c1bf5d4fd994628ea6a6b504968f
Melanie Vogel <melanie.vogel@siemens-healthineers.com>
2021-10-21 14:43:42 +0200

    feat: Added SumOfOddNumbers.

---
+++ CSharpKatas.Tests/7Kyu/SumOfOddNumbersTests.cs
@@ -0,0 +1,32 @@
+﻿using CSharp_Katas._7Kyu;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp_Katas_Tests._7Kyu
+{
+    [TestClass]
+    public class SumOfOddNumbersTests
+    {
+        /*
+         *              1
+         *             3 5
+         *            7 9 11
+         *         13 15 17 19
+         *        ...
+         */
+        [DataRow(1, 1)]
+        [DataRow(2, 8)]
+        [DataRow(3, 27)]
+        [DataTestMethod]
+        public void RowSumOddNumbers_Should_Return_Expected(long input, long expected)
+        {
+            // Arrange
+            // see DataRow
+
+            // Act
+            var actual = SumOfOddNumbers.RowSumOddNumbers(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ CSharpKatas/7Kyu/SumOfOddNumbers.cs
@@ -0,0 +1,28 @@
+﻿namespace CSharp_Katas._7Kyu
+{
+    public class SumOfOddNumbers
+    {
+        /*
+         *              1
+         *             3 5
+         *            7 9 11
+         *         13 15 17 19
+         *        ...
+         */
+        public static long RowSumOddNumbers(long row)
+        {
+            var firstNumberOfRowN = row * (row - 1) + 1;
+            var lastNumberOfRowN = firstNumberOfRowN + (row * 2);
+
+            long r = 0;
+            for (long i = firstNumberOfRowN; i < lastNumberOfRowN; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    r += i;
+                }
+            }
+            return r;
+        }
+    }
+}
