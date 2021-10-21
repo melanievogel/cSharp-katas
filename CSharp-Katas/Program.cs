5bccb7b9b3a99edb947360e5d35693efbefe8fa6
Melanie Vogel <melanie.vogel@siemens-healthineers.com>
2021-10-21 11:28:32 +0200

    chore: inital commit.

---
+++ CSharp-Katas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Katas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
