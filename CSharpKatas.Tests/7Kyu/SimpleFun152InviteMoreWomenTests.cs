7b848b6c357f49d3c1ae4f13730c9745b16427e1
Melanie Vogel <melanie.vogel@siemens-healthineers.com>
2021-10-21 14:59:33 +0200

    chore: Removed cached mess.

---
+++ CSharpKatas.Tests/7Kyu/SimpleFun152InviteMoreWomenTests.cs
@@ -0,0 +1,27 @@
+﻿using CSharp_Katas._7Kyu;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp_Katas_Tests._7Kyu
+{
+    [TestClass]
+    public class SimpleFun152InviteMoreWomenTests
+    {
+        [DataRow(new int[]{ 1, -1, 1 }, true)]
+        [DataRow(new int[]{ 1, 1, 1 }, true)]
+        [DataRow(new int[]{ -1, -1, -1 }, false)]
+        [DataRow(new int[]{ 1, -1 }, false)]
+        [DataTestMethod]
+        public void InviteMoreWomen_Should_Return_Expected(int[] input, bool expected)
+        {
+            // Arrange
+            // --> DataRows
+
+            // Act
+            var actual = SimpleFun152InviteMoreWomen.InviteMoreWomen(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
---
+++ CSharpKatas/7Kyu/SimpleFun152InviteMoreWomen.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace CSharp_Katas._7Kyu
+{
+    public class SimpleFun152InviteMoreWomen
+    {
+        public static bool InviteMoreWomen(int[] L)
+        {
+            return L.Where(w => w == -1).Count() >= L.Where(m => m == 1).Count() ? false : true;
+        }
+    }
+}
