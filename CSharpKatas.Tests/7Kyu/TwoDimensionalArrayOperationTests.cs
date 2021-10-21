ce607267a618afceb9e7bf3f06cacc709472e1c0
Melanie Vogel <melanie.vogel@siemens-healthineers.com>
2021-10-21 17:00:33 +0200

    feat: Added twodim array ops kata.

---
+++ CSharpKatas.Tests/7Kyu/TwoDimensionalArrayOperationTests.cs
@@ -0,0 +1,52 @@
+﻿using CSharp_Katas._7Kyu;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp_Katas_Tests._7Kyu
+{
+    [TestClass]
+    public class TwoDimensionalArrayOperationTests
+    {
+        
+        [TestMethod]
+        public void Result_Should_Return_4()
+        {
+            // Arrange
+            var input = new int[,] {{4}};
+            var expected = 4;
+
+            // Act
+            var actual = TwoDimensionalArrayOperation.Result(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void Result_Should_Return_21()
+        {
+            // Arrange
+            var input = new int[,] { { 1, 2 }, { 3, 4 } };
+            var expected = 21;
+
+            // Act
+            var actual = TwoDimensionalArrayOperation.Result(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void Result_Should_Return_5376()
+        {
+            // Arrange
+            var input = new int[,] { { 7, 1, 5, 8 }, { 2, 1, 4, 9 }, { 3, 2, 4, 7 } };
+            var expected = 5376;
+
+            // Act
+            var actual = TwoDimensionalArrayOperation.Result(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ CSharpKatas/7Kyu/TwoDimensionalArrayOperation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp_Katas._7Kyu
+{
+    public class TwoDimensionalArrayOperation
+    {
+        public static int Result(int[,] array)
+        {
+            int sum = 0;
+            int product = 1;
+
+            List<int> sumList = new List<int>();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    sum += array[i, j];
+                }
+
+                sumList.Add(sum);
+                sum = 0;
+            }
+
+            foreach (var v in sumList)
+            {
+                product *= v;
+            }
+
+            return product;
+        }
+    }
+}
